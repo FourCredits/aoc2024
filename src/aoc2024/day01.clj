77724f917964d57653b0f8a420c679e473569c2b
Erik Grundy <erik.grundy@gmail.com>
2024-12-01 19:08:07 +1300

    day 1

--- src/aoc2024/day01.clj
+++ src/aoc2024/day01.clj
@@ -1,5 +1,25 @@
-(ns aoc2024.day01)
+(ns aoc2024.day01
+  (:require
+   [clojure.string :as str]))
 
-(defn part1 [input] :todo)
+(defn parse [input]
+  (->> input
+       str/split-lines
+       (map #(map parse-long (str/split % #" +")))
+       (apply map vector)))
 
-(defn solve [input] [])
+(defn part1 [lists]
+  (->> lists
+       (map sort)
+       (apply map (comp abs -))
+       (apply +)))
+
+(defn count-occurrences [coll]
+  (reduce (fn [acc elem] (update acc elem #(inc (or % 0)))) {} coll))
+
+(defn part2 [[l1 l2]]
+  (let [occurences-in-l2 (count-occurrences l2)
+        similarity-score (fn [x] (* x (get occurences-in-l2 x 0)))]
+    (apply + (map similarity-score l1))))
+
+(defn solve [input] ((juxt part1 part2) (parse input)))
--- test/aoc2024/day01_test.clj
+++ test/aoc2024/day01_test.clj
@@ -1,13 +1,29 @@
 (ns aoc2024.day01-test
   (:require [clojure.test :refer [deftest is run-tests testing]]
             [clojure.java.io :as io]
-            [aoc2024.day01 :refer [part1]]))
+            [aoc2024.day01 :refer [part1 part2 parse]]))
+
+(def ^:private example
+  "3   4
+4   3
+2   5
+1   3
+3   9
+3   3")
+
+(def ^:private real-input (slurp (io/resource "day01.txt")))
 
 (deftest part1-tests
   (testing "example data"
-    (is (= (part1 "") :wrong)))
-  (testing "real thing"
-    (is (= (part1 (slurp (io/resource "day01.txt"))) :wrong))))
+    (is (= (part1 (parse example)) 11)))
+  (testing "real input"
+    (is (= (part1 (parse real-input)) 2192892))))
+
+(deftest part2-tests
+  (testing "example data"
+    (is (= (part2 (parse example)) 31)))
+  (testing "real input"
+    (is (= (part2 (parse real-input)) 22962826))))
 
 (comment
   (run-tests))
