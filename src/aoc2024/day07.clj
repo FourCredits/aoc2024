ea9a67937c5ab90b881f88e0da6d974c58b35645
Erik Grundy <erik.grundy@gmail.com>
2024-12-08 01:54:50 +1300

    day 7 part 2

--- src/aoc2024/day07.clj
+++ src/aoc2024/day07.clj
@@ -3,6 +3,13 @@
    [aoc2024.file-inputs :as file-inputs]
    [clojure.string :as str]))
 
+(defn parse [input]
+  (->> input
+       str/split-lines
+       (map #(let [[target rhs] (str/split % #": ")]
+               {:target (parse-long target)
+                :rhs (map parse-long (str/split rhs #" "))}))))
+
 (defn- options [numbers]
   (if (= 1 (count numbers))
     numbers
@@ -10,35 +17,23 @@
           r-options (options r)]
       (concat (map #(* f %) r-options) (map #(+ f %) r-options)))))
 
-(comment
-  (let [input (file-inputs/example-file 7)]
-    (let [parse-line (fn [line]
-                       (let [[target rhs] (str/split line #": ")]
-                         {:target (parse-long target)
-                          :rhs (map parse-long (str/split rhs #" "))}))]
-      (->> input
-           str/split-lines
-           (map parse-line)
-           (keep (fn [{:keys [target rhs]}]
-                   (->> rhs
-                        reverse
-                        options
-                        (some #{target}))))
-           (apply +)))))
+(defn- options2 [numbers]
+  (condp #(= %1 (count %2)) numbers
+    0 []
+    1 numbers
+    (for [operation [+ * (fn [x y] (parse-long (str x y)))]
+          :let [[a b & c] numbers]
+          option (options2 (cons (operation a b) c))]
+      option)))
 
-(defn part1 [input]
-  (let [parse-line (fn [line]
-                     (let [[target rhs] (str/split line #": ")]
-                       {:target (parse-long target)
-                        :rhs (map parse-long (str/split rhs #" "))}))]
-    (->> input
-         str/split-lines
-         (map parse-line)
-         (keep (fn [{:keys [target rhs]}]
-                 (->> rhs reverse options (some #{target}))))
-         (apply +))))
+(defn part1 [equations]
+  (->> equations
+       (keep (fn [{:keys [target rhs]}] (->> rhs reverse options (some #{target}))))
+       (apply +)))
 
-(defn part2 [input]
-  :todo)
+(defn part2 [equations]
+  (->> equations
+       (keep (fn [{:keys [target rhs]}] (some #{target} (options2 rhs))))
+       (apply +)))
 
 (defn solve [input] ((juxt part1 part2) input))
--- test/aoc2024/day07_test.clj
+++ test/aoc2024/day07_test.clj
@@ -1,19 +1,17 @@
 (ns aoc2024.day07-test
   (:require
-   [aoc2024.day07 :refer [part1 part2]]
+   [aoc2024.day07 :refer [parse part1 part2]]
    [aoc2024.file-inputs :as file-inputs]
    [clojure.test :refer [deftest is testing]]))
 
 (deftest ^:example example
-  (let [example-input (file-inputs/example-file 7)]
+  (let [example-input (parse (file-inputs/example-file 7))]
     (testing "example input"
-      (is (= (part1 example-input) 3749)))))
+      (is (= (part1 example-input) 3749))
+      (is (= (part2 example-input) 11387)))))
 
 (deftest ^:real real
-  (let [real-input (file-inputs/real-file 7)]
+  (let [real-input (parse (file-inputs/real-file 7))]
     (testing "real input"
-      (is (= (part1 real-input) 3119088655389)))))
-
-(comment
-  (part2 (file-inputs/real-file 7)) ; 3119088655389
-  (clojure.test/run-tests))
+      (is (= (part1 real-input) 3119088655389))
+      (is (= (part2 real-input) 264184041398847)))))
