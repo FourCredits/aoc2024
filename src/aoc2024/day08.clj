14d25b567a8278a3222b9c0a7046d8e21cf28d75
Erik Grundy <erik.grundy@gmail.com>
2024-12-08 18:55:48 +1300

    extract common parsing

--- src/aoc2024/day08.clj
+++ src/aoc2024/day08.clj
@@ -3,6 +3,14 @@
    [aoc2024.two-d :as two-d]
    [clojure.string :as str]))
 
+(defn parse [input]
+  (let [grid (two-d/grid-seq (str/split-lines input))
+        [[max-r max-c] _] (last grid)
+        on-map? (fn [[r c]] (and (<= 0 r max-r) (<= 0 c max-c)))
+        grouping (group-by second (remove #(= \. (second %)) grid))
+        by-frequency (->> grouping vals (map #(mapv first %)))]
+    {:by-frequency by-frequency :on-map? on-map?}))
+
 (defn all-pairs-no-swaps [coll]
   (for [i (range (count coll))
         j (range (inc i) (count coll))]
@@ -16,19 +24,10 @@
            [(+ r1 (* 2 r-diff)) (+ c1 (* 2 c-diff))]]]
     n))
 
-(defn part1 [input]
-  (let [grid (two-d/grid-seq (str/split-lines input))
-        [[max-r max-c] _] (last grid)
-        on-map? (fn [[r c]] (and (<= 0 r max-r) (<= 0 c max-c)))
-        grouping (group-by second (remove #(= \. (second %)) grid))
-        by-frequency (-> grouping (update-vals #(mapv first %)))]
-    (->> by-frequency
-         (mapcat (comp antinodes second))
-         (filter on-map?)
-         distinct
-         count)))
+(defn part1 [{:keys [by-frequency on-map?]}]
+  (->> by-frequency (mapcat antinodes) (filter on-map?) distinct count))
 
-(defn part2 [input]
+(defn part2 [{:keys [by-frequency on-map?]}]
   :todo)
 
-(defn solve [input] ((juxt part1 part2) input))
+(defn solve [input] ((juxt part1 part2) (parse input)))
--- test/aoc2024/day08_test.clj
+++ test/aoc2024/day08_test.clj
@@ -1,17 +1,17 @@
 (ns aoc2024.day08-test
   (:require
-   [aoc2024.day08 :refer [part1]]
+   [aoc2024.day08 :refer [parse part1]]
    [aoc2024.file-inputs :as file-inputs]
    [clojure.test :refer [deftest is testing]]))
 
 (deftest ^:example example
   (testing "example data"
-    (let [input (file-inputs/example-file 8)]
+    (let [input (parse (file-inputs/example-file 8))]
       (is (= (part1 input) 14)))))
 
 (deftest ^:real real
   (testing "real data"
-    (let [input (file-inputs/real-file 8)]
+    (let [input (parse (file-inputs/real-file 8))]
       (is (= (part1 input) 240)))))
 
 (comment
