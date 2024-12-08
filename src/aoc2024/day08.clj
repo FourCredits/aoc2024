3b560ae2e97177dcda48d3de352e592e30c10c29
Erik Grundy <erik.grundy@gmail.com>
2024-12-08 19:21:51 +1300

    day 8 part 2

--- src/aoc2024/day08.clj
+++ src/aoc2024/day08.clj
@@ -16,18 +16,33 @@
         j (range (inc i) (count coll))]
     [(get coll i) (get coll j)]))
 
+(defn distance-between [[r1 c1] [r2 c2]] [(- r2 r1) (- c2 c1)])
+
+(defn scale [n [r c]] [(* r n) (* c n)])
+
+(defn add-vectors [[r1 c1] [r2 c2]] [(+ r1 r2) (+ c1 c2)])
+
+(defn nth-harmonic [n v diff] (add-vectors v (scale n diff)))
+
 (defn antinodes [positions]
-  (for [[[r1 c1] [r2 c2]] (all-pairs-no-swaps positions)
-        :let [r-diff (- r2 r1)
-              c-diff (- c2 c1)]
-        n [[(+ r1 (* -1 r-diff)) (+ c1 (* -1 c-diff))]
-           [(+ r1 (* 2 r-diff)) (+ c1 (* 2 c-diff))]]]
+  (for [[p1 p2] (all-pairs-no-swaps positions)
+        :let [difference (distance-between p1 p2)]
+        n [(nth-harmonic -1 p1 difference) (nth-harmonic 2 p1 difference)]]
     n))
 
 (defn part1 [{:keys [by-frequency on-map?]}]
   (->> by-frequency (mapcat antinodes) (filter on-map?) distinct count))
 
+(defn antinodes2 [on-map? positions]
+  (for [[p1 p2] (all-pairs-no-swaps positions)
+        :let [difference (distance-between p1 p2)
+              nth-harmonic (fn [n] (nth-harmonic n p1 difference))]
+        n (concat
+           (take-while on-map? (map nth-harmonic (iterate inc 0)))
+           (take-while on-map? (map nth-harmonic (iterate dec 0))))]
+    n))
+
 (defn part2 [{:keys [by-frequency on-map?]}]
-  :todo)
+  (->> by-frequency (mapcat #(antinodes2 on-map? %)) distinct count))
 
 (defn solve [input] ((juxt part1 part2) (parse input)))
--- test/aoc2024/day08_test.clj
+++ test/aoc2024/day08_test.clj
@@ -1,18 +1,20 @@
 (ns aoc2024.day08-test
   (:require
-   [aoc2024.day08 :refer [parse part1]]
+   [aoc2024.day08 :refer [parse part1 part2]]
    [aoc2024.file-inputs :as file-inputs]
    [clojure.test :refer [deftest is testing]]))
 
 (deftest ^:example example
   (testing "example data"
     (let [input (parse (file-inputs/example-file 8))]
-      (is (= (part1 input) 14)))))
+      (is (= (part1 input) 14))
+      (is (= (part2 input) 34)))))
 
 (deftest ^:real real
   (testing "real data"
     (let [input (parse (file-inputs/real-file 8))]
-      (is (= (part1 input) 240)))))
+      (is (= (part1 input) 240))
+      (is (= (part2 input) 955)))))
 
 (comment
   (clojure.test/run-tests))
