bab141acedda6496ba3ec039707bc63f8160b1af
Erik Grundy <erik.grundy@gmail.com>
2024-12-08 22:38:09 +1300

    simplify and abstract common elements

--- src/aoc2024/day08.clj
+++ src/aoc2024/day08.clj
@@ -13,30 +13,19 @@
         j (range (inc i) (count coll))]
     [(get coll i) (get coll j)]))
 
-(defn nth-harmonic [n v diff] (two-d/add-vectors v (two-d/scale n diff)))
+(defn- nth-harmonic [v diff n] (two-d/add-vectors v (two-d/scale n diff)))
 
-(defn without-resonance [on-map? [p1 p2]]
-  (let [difference (two-d/distance-between p1 p2)]
-    (->> [-1 2] (map #(nth-harmonic % p1 difference)) (filter on-map?))))
+(defn- antinodes [{:keys [on-map? by-frequency]} resonance-vectors]
+  (count (distinct (for [positions by-frequency
+                         [p1 p2] (all-pairs-no-swaps positions)
+                         :let [diff (two-d/distance-between p1 p2)]
+                         resonance-vector resonance-vectors
+                         harmonic resonance-vector
+                         :let [pos (nth-harmonic p1 diff harmonic)]
+                         :while (on-map? pos)]
+                     pos))))
 
-(defn with-resonance [on-map? [p1 p2]]
-  (let [diff (two-d/distance-between p1 p2)]
-    (->> [(iterate inc 0) (iterate dec 0)]
-         (mapcat (fn [direction]
-                   (->> direction
-                        (map #(nth-harmonic % p1 diff))
-                        (take-while on-map?)))))))
-
-(defn antinodes [resonance positions]
-  (mapcat resonance (all-pairs-no-swaps positions)))
-
-(defn all-antinodes [resonance by-frequency]
-  (->> by-frequency (mapcat #(antinodes resonance %)) distinct count))
-
-(defn part1 [{:keys [by-frequency on-map?]}]
-  (all-antinodes #(without-resonance on-map? %) by-frequency))
-
-(defn part2 [{:keys [by-frequency on-map?]}]
-  (all-antinodes #(with-resonance on-map? %) by-frequency))
+(defn part1 [args] (antinodes args [[-1] [2]]))
+(defn part2 [args] (antinodes args [(iterate dec -1) (iterate inc 0)]))
 
 (defn solve [input] ((juxt part1 part2) (parse input)))
