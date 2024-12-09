11f4546b49063abb5b49cec6bc28cff519e3842f
Erik Grundy <erik.grundy@gmail.com>
2024-12-10 07:28:54 +1300

    day 9 part 2

--- src/aoc2024/day09.clj
+++ src/aoc2024/day09.clj
@@ -1,36 +1,65 @@
 (ns aoc2024.day09
   (:require
-   [aoc2024.file-inputs :as file-inputs]
    [clojure.string :as str]))
 
 (defn- char->int [element] (- (int element) (int \0)))
 
-(defn- format-filesystem [filesystem]
-  (loop [fs filesystem
-         head 0
-         tail (dec (count filesystem))]
-    (let [hblock (fs head)
-          tblock (fs tail)]
-      (cond
-        (>= head tail) fs
-        (some? hblock) (recur fs (inc head) tail)
-        (nil? tblock) (recur fs head (dec tail))
-        :else (recur (assoc fs head tblock tail hblock)
-                     (inc head)
-                     (dec tail))))))
-
 (defn- id [index] (when (even? index) (quot index 2)))
 
-(defn- mk-region [index n] (repeat (char->int n) (id index)))
-
-(defn checksum [fs]
-  (transduce (comp (take-while some?) (map-indexed *)) + fs))
+(defn- format-filesystem [filesystem]
+  (loop [fs (transient filesystem), h 0, t (dec (count filesystem))]
+    (cond
+      (>= h t) (persistent! fs)
+      (some? (fs h)) (recur fs (inc h) t)
+      (nil? (fs t)) (recur fs h (dec t))
+      :else (recur (assoc! fs h (fs t) t (fs h)) (inc h) (dec t)))))
 
 (defn part1 [input]
-  (let [fs (into [] (comp (map-indexed mk-region) cat) (str/trim input))]
-    (checksum (format-filesystem fs))))
+  (let [mk-region (fn [index n] (repeat (char->int n) (id index)))
+        fs (into [] (comp (map-indexed mk-region) cat) (str/trim input))
+        checksum (comp (take-while some?) (map-indexed *))]
+    (transduce checksum + (format-filesystem fs))))
+
+(defn- map-accum
+  ([f initial-state]
+   (fn [rf]
+     (let [state (volatile! initial-state)]
+       (fn
+         ([] (rf))
+         ([result input]
+          (let [[new-state input] (f @state input)]
+            (vreset! state new-state)
+            (rf result input)))
+         ([result] (rf result))))))
+  ([f initial-state coll]
+   (lazy-seq
+    (when-let [[a & as] (seq coll)]
+      (let [[next-state b] (f initial-state a)]
+        (cons b (map-accum f next-state as)))))))
+
+(defn- fits? [file {:keys [start size] :as space}]
+  (when (and (>= size (:size file)) (< start (:start file)))
+    [space {:size (- size (:size file)) :start (+ start (:size file))}]))
+
+(defn- checksum-file [{:keys [start size id]}]
+  (* id (/ (* size (+ (* start 2) size -1)) 2)))
+
+(defn- fit-file-in [free-spaces file]
+  (if-let [[old new] (some #(fits? file %) free-spaces)]
+    [(-> free-spaces (disj old) (conj new))
+     (checksum-file (assoc file :start (:start old)))]
+    [free-spaces (checksum-file file)]))
+
+(defn- mk-region [offset [index x]]
+  (let [s (char->int x)]
+    [(+ offset s) {:start offset :size s :id (id index)}]))
+
+(defn- compare-by [keyfn] (fn [x1 x2] (< (keyfn x1) (keyfn x2))))
 
 (defn part2 [input]
-  :todo)
+  (let [regions (map-accum mk-region 0 (map-indexed vector (str/trim input)))
+        {free-spaces false files true} (group-by (comp some? :id) regions)
+        free-spaces (apply sorted-set-by (compare-by :start) free-spaces)]
+    (transduce (map-accum fit-file-in free-spaces) + (reverse files))))
 
 (defn solve [input] ((juxt part1 part2) input))
--- test/aoc2024/day09_test.clj
+++ test/aoc2024/day09_test.clj
@@ -1,15 +1,17 @@
 (ns aoc2024.day09-test
   (:require
-   [aoc2024.day09 :refer [part1]]
+   [aoc2024.day09 :refer [part1 part2]]
    [aoc2024.file-inputs :as file-inputs]
    [clojure.test :refer [deftest is testing]]))
 
 (deftest ^:example example
   (let [input (file-inputs/example-file 9)]
     (testing "example input"
-      (is (= (part1 input) 1928)))))
+      (is (= (part1 input) 1928))
+      (is (= (part2 input) 2858)))))
 
-(deftest ^:real example
+(deftest ^:real real
   (let [input (file-inputs/real-file 9)]
     (testing "real input"
-      (is (= (part1 input) 6367087064415)))))
+      (is (= (part1 input) 6367087064415))
+      (is (= (part2 input) 6390781891880)))))
