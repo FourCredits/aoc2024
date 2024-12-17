0631609fce18472dde48809f56b3d3897e82263c
Erik Grundy <erik.grundy@alstefgroup.com>
2024-12-17 23:19:03 +1300

    some day 15 refactoring

--- src/aoc2024/day15.clj
+++ src/aoc2024/day15.clj
@@ -11,87 +11,76 @@
 
 (defn parse [input]
   (let [[grid instructions] (str/split input #"\n\n")
-        directions (keep char->direction instructions)
         cell-types (->> grid str/split-lines
                         two-d/grid
                         two-d/group-positions-by-value)]
     {:robot (first (cell-types \@))
      :boxes (set (cell-types \O))
      :walls (set (cell-types \#))
-     :directions directions}))
-
-(defn- make-space
-  "try to move boxes in the specified direction such that pos becomes available.
-  returns nil if moving the boxes in this way is impossible"
-  [walls pos direction boxes]
-  (let [pos' (direction pos)]
-    (cond
-      (walls pos) nil
-      (boxes pos) (when-let [boxes' (make-space walls pos' direction boxes)]
-                    (-> boxes' (disj pos) (conj pos')))
-      :else boxes)))
-
-(defn- move [{:keys [walls boxes robot] :as state} direction]
-  (if-let [boxes' (make-space walls (direction robot) direction boxes)]
-    (assoc state :boxes boxes' :robot (direction robot))
-    state))
-
-(defn- simulate [{:keys [directions] :as state}] (reduce move state directions))
+     :directions (keep char->direction instructions)}))
 
 (defn- score [{:keys [boxes]}]
   (transduce (map (fn [[r c]] (+ (* 100 r) c))) + boxes))
 
-(defn part1 [input] (->> input parse simulate score))
-
-(defn- to-big-coords [[r c]] [r (* c 2)])
-
-(defn- duplicate [p] [p (two-d/right p)])
-
 (defn- embiggen [{:keys [walls directions boxes robot]}]
-  {:robot (to-big-coords robot)
-   :boxes (into #{} (map to-big-coords) boxes)
-   :walls (into #{} (mapcat (comp duplicate to-big-coords)) walls)
-   :directions directions})
+  (letfn [(to-big-coords [[r c]] [r (* c 2)])
+          (duplicate [p] [p (two-d/right p)])]
+    {:robot (to-big-coords robot)
+     :boxes (into #{} (map to-big-coords) boxes)
+     :walls (into #{} (mapcat (comp duplicate to-big-coords)) walls)
+     :directions directions}))
 
-(defn- replace-with
-  "takes a set s, removes old, and adds in new"
-  [s old new]
-  (-> s (disj old) (conj new)))
+(defn- swap [s old new] (-> s (disj old) (conj new)))
 
-(defn- make-space2 [boxes walls pos direction]
-  (let [pos' (direction pos)]
-    (cond
-      (walls pos) nil
-      (= direction two-d/right) (if (boxes pos)
-                                  (some-> boxes
-                                          (make-space2 walls (direction pos') direction)
-                                          (replace-with pos pos'))
-                                  boxes)
-      (= direction two-d/left) (if (boxes pos')
-                                 (let [pos'' (direction pos')]
-                                   (some-> boxes
-                                           (make-space2 walls pos'' direction)
-                                           (replace-with pos' pos'')))
-                                 boxes)
-      (boxes pos) (some-> boxes
-                          (make-space2 walls pos' direction)
-                          (make-space2 walls (two-d/right pos') direction)
-                          (replace-with pos pos'))
-      (boxes (two-d/left pos)) (some-> boxes
-                                       (make-space2 walls pos' direction)
-                                       (make-space2 walls (two-d/left pos') direction)
-                                       (replace-with (two-d/left pos) (two-d/left pos')))
-      :else boxes)))
+(defn- move-boxes1 [walls]
+  (fn [direction]
+    (fn inner [boxes p]
+      (let [p' (direction p)]
+        (cond
+          (walls p) nil
+          (boxes p) (some-> (inner boxes p') (swap p p'))
+          :else boxes)))))
 
-(defn- move2 [{:keys [walls boxes robot] :as state} direction]
-  (if-let [boxes' (make-space2 boxes walls (direction robot) direction)]
-    (assoc state :boxes boxes' :robot (direction robot))
-    state))
+(defn- move-boxes2 [walls]
+  (fn [direction]
+    (condp = direction
+      two-d/right (fn inner [boxes p]
+                    (let [p' (direction p)
+                          p'' (direction p')]
+                      (cond
+                        (walls p) nil
+                        (boxes p) (some-> boxes (inner p'') (swap p p'))
+                        :else boxes)))
+      two-d/left (fn inner [boxes p]
+                   (let [p' (direction p)
+                         p'' (direction p')]
+                     (cond
+                       (walls p) nil
+                       (boxes p') (some-> boxes (inner p'') (swap p' p''))
+                       :else boxes)))
+      (fn inner [boxes p]
+        (let [p' (direction p)]
+          (cond
+            (walls p) nil
+            (boxes p) (some-> boxes
+                              (inner p')
+                              (inner (two-d/right p'))
+                              (swap p p'))
+            (boxes (two-d/left p)) (some-> boxes
+                                           (inner p')
+                                           (inner (two-d/left p'))
+                                           (swap (two-d/left p) (two-d/left p')))
+            :else boxes))))))
 
-(defn- simulate2 [{:keys [directions] :as state}]
-  (reduce move2 state directions))
+(defn- simulate-2 [make-space {:keys [directions walls] :as state}]
+  (let [f (make-space walls)
+        move (fn [{:keys [boxes robot] :as state} direction]
+               (if-let [boxes' ((f direction) boxes (direction robot))]
+                 (assoc state :boxes boxes' :robot (direction robot))
+                 state))]
+    (reduce move state directions)))
 
-;; TODO: move parse out of both functions?
-(defn part2 [input] (-> input parse embiggen simulate2 score))
+(defn part1 [input] (->> input          (simulate-2 move-boxes1) score))
+(defn part2 [input] (->> input embiggen (simulate-2 move-boxes2) score))
 
-(defn solve [input] ((juxt part1 part2) input))
+(defn solve [input] ((juxt part1 part2) (parse input)))
--- test/aoc2024/day15_test.clj
+++ test/aoc2024/day15_test.clj
@@ -1,17 +1,20 @@
 (ns aoc2024.day15-test
   (:require
-   [aoc2024.day15 :refer [part1 part2]]
-   [clojure.test :refer [deftest is testing]]
-   [aoc2024.file-inputs :as file-inputs]))
+   [aoc2024.day15 :refer [parse part1 part2]]
+   [aoc2024.file-inputs :as file-inputs]
+   [clojure.test :refer [deftest is testing]]))
 
 (deftest ^:example example
   (testing "example data"
-    (let [input (file-inputs/example-file 15)]
+    (let [input (parse (file-inputs/example-file 15))]
       (is (= (part1 input) 10092))
       (is (= (part2 input) 9021)))))
 
 (deftest ^:real real
   (testing "real data"
-    (let [input (file-inputs/real-file 15)]
+    (let [input (parse (file-inputs/real-file 15))]
       (is (= (part1 input) 1577255))
       (is (= (part2 input) 1597035)))))
+
+(comment
+  (clojure.test/run-tests))
