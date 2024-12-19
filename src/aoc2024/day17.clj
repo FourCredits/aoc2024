958518eedd90d29f6c4ec71238d3f6c8cb70792e
Erik Grundy <erik.grundy@gmail.com>
2024-12-19 17:31:55 +1300

    WIP

--- src/aoc2024/day17.clj
+++ src/aoc2024/day17.clj
@@ -1,9 +1,19 @@
 (ns aoc2024.day17
   (:require
    [clojure.math :as math]
-   [clojure.string :as str]))
+   [clojure.string :as str]
+   [aoc2024.file-inputs :as file-inputs]))
 
-(defn combo-operand [{:keys [a b c]} operand]
+(defn parse [input]
+  (let [[a b c & program] (map parse-long (re-seq #"\d+" input))
+        program (vec program)
+        program-end (count program)
+        end? #(>= (inc %) program-end)
+        fetch (fn [{:keys [pc]}]
+                (when (not (end? pc)) (subvec program pc (+ pc 2))))]
+    {:a a :b b :c c :pc 0 :output [] :program program :fetch fetch}))
+
+(defn- combo-operand [{:keys [a b c]} operand]
   (case operand
     (0 1 2 3) operand
     4 a
@@ -23,16 +33,26 @@
       6 (assoc registers :pc pc' :b (int (/ a (math/pow 2 (combo-operand registers operand)))))
       7 (assoc registers :pc pc' :c (int (/ a (math/pow 2 (combo-operand registers operand))))))))
 
-(defn part1 [input]
-  (let [[a b c & program] (map parse-long (re-seq #"\d+" input))
-        program (vec program)
-        program-end (count program)
-        end? #(>= % program-end)]
-    (loop [{:keys [output pc] :as computer} {:a a :b b :c c :pc 0 :output []}]
-      (if-let [ins (when (not (end? pc)) (subvec program pc (+ pc 2)))]
-        (recur (execute-instruction computer ins))
-        (str/join \, output)))))
+(defn- step [{:keys [fetch] :as computer}]
+  (if-let [instruction (fetch computer)]
+    (execute-instruction computer instruction)
+    (assoc computer :halted? true)))
 
-(defn part2 [input] :todo)
+(defn- run-and-get-output [computer]
+  (->> computer
+       (iterate step)
+       (filter :halted?)
+       first
+       :output))
 
-(defn solve [input] ((juxt part1 part2) input))
+(defn part1 [computer] (str/join \, (run-and-get-output computer)))
+
+(comment
+  (bit-xor 3 5))
+
+(defn part2 [computer]
+  (->> (range)
+       (filter #(= (run-and-get-output (assoc computer :a %)) (:program computer)))
+       first))
+
+(defn solve [input] ((juxt part1 part2) (parse input)))
--- test/aoc2024/day17_test.clj
+++ test/aoc2024/day17_test.clj
@@ -1,19 +1,17 @@
 (ns aoc2024.day17-test
   (:require
-   [aoc2024.day17 :refer [part1 part2]]
+   [aoc2024.day17 :refer [parse part1 part2]]
    [aoc2024.file-inputs :as file-inputs]
    [clojure.test :refer [deftest is testing]]))
 
 (deftest ^:example example
-  (let [input (file-inputs/example-file 17)]
-    (testing "example input"
-      (is (= (part1 input) "4,6,3,5,6,3,5,2,1,0")))))
+  (testing "example input"
+    (is (= (part1 (parse (file-inputs/example-file 17 1)))
+           "4,6,3,5,6,3,5,2,1,0"))
+    (is (= (part2 (parse (file-inputs/example-file 17 2))) 117440))))
 
 (deftest ^:real real
-  (let [input (file-inputs/real-file 17)]
+  (let [input (parse (file-inputs/real-file 17))]
     (testing "real input"
-      (is (= (part1 input) "6,2,7,2,3,1,6,0,5")))))
-
-(comment
-  (part1 (file-inputs/real-file 17)) ; "6,2,7,2,3,1,6,0,5"
-  (clojure.test/run-tests))
+      (is (= (part1 input) "6,2,7,2,3,1,6,0,5"))
+      #_(is (= (part2 input) :placeholder)))))
